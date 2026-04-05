d4d5336fb89c47b1e694db134143f4418f674044
ruvcaltd <ruvcaltd@gmail.com>
2026-04-05 21:00:04 +0100

    db

--- postgres/init/02-docuseal-db.sh
+++ postgres/init/02-docuseal-db.sh
@@ -2,6 +2,9 @@
 set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "CREATE DATABASE docuseal;"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "CREATE USER documenso_user WITH PASSWORD '$DOCUMENSO_DB_PASSWORD';"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "ALTER USER documenso_user CREATEDB;"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "ALTER USER documenso_user CREATEROLE;"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "ALTER USER documenso_user REPLICATION;"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "ALTER DATABASE docuseal OWNER TO documenso_user;"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d docuseal -c "GRANT ALL ON SCHEMA public TO documenso_user;"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d docuseal -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO documenso_user;"
