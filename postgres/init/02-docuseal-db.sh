f4caa1a5ecb1707c98b4a5e2608250f6414b0407
ruvcaltd <ruvcaltd@gmail.com>
2026-04-05 20:52:25 +0100

    test

--- postgres/init/02-docuseal-db.sh
+++ postgres/init/02-docuseal-db.sh
@@ -3,3 +3,8 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "CREATE DATABASE docuseal;"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "CREATE USER documenso_user WITH PASSWORD '$DOCUMENSO_DB_PASSWORD';"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "GRANT ALL PRIVILEGES ON DATABASE docuseal TO documenso_user;"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d docuseal -c "GRANT ALL ON SCHEMA public TO documenso_user;"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d docuseal -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO documenso_user;"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d docuseal -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO documenso_user;"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d docuseal -c "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO documenso_user;"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d docuseal -c "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO documenso_user;"
